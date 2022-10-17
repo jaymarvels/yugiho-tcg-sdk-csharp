6437bdbc20d2b09330c7a8992db0390854dd95e6
Jacob Spencer <jacob.spencer80@googlemail.com>
2022-10-17 22:43:15 +0100

    Added in monster variant POCO classes

---
+++ YuGiHoTcgSdk/Infrastructure/HttpClients/Cards/Monster/FusionMonster.cs
@@ -0,0 +1,8 @@
+﻿using YuGiHoTcgSdk.Infrastructure.HttpClients.Common;
+
+namespace YuGiHoTcgSdk.Infrastructure.HttpClients.Cards.Monster;
+
+public class FusionMonster : Monster
+{
+    internal new static string ApiEndpoint { get; } = $"{Global.CardApiBase}?type={Global.Monster.Fusion}";
+}
