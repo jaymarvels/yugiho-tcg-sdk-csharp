fc9723dd60eac723b6ef802e1aff07449a834feb
Jacob Spencer <jacob.spencer80@googlemail.com>
2022-10-19 15:58:02 +0100

    Added in archetypes

---
+++ YuGiHoTcgSdk/Infrastructure/HttpClients/Archetype/Archetypes.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using YuGiHoTcgSdk.Infrastructure.HttpClients.Common;
+
+namespace YuGiHoTcgSdk.Infrastructure.HttpClients.Archetype;
+
+public class Archetypes : ResourceBase
+{
+    internal new static string ApiEndpoint { get; } = Global.ArchetypeApiBase;
+
+    public override string Id { get; set; }
+
+    [JsonProperty("archetype_name")]
+    public string ArchetypeName { get; set; }
+}
